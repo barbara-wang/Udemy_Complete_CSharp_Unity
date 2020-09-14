399b6141051a8a7005cfae335776633c94436ce2
Barbara Wang <barbara.wang@unity3d.com>
2020-09-13 22:30:00 -0400

    Laser Defender, my third game

---
+++ Unity2D/Lazer_Defender/Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // config params
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for(int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            var currentWave = waveConfigs[waveIndex];
+            yield return SpawnAllEnemiesInWave(currentWave);
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig wave)
+    {
+        for (int enemyCount = 0; enemyCount < wave.GetNumberOfEnemies(); enemyCount++)
+        { 
+            var newEnemy = Instantiate(wave.GetEnemyPrefab(),
+                                    wave.GetWaypoints()[0].transform.position,
+                                    Quaternion.identity);
+
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(wave);
+            yield return new WaitForSeconds(wave.GetTimeBetweenSpawns());
+        }
+    }
+}
