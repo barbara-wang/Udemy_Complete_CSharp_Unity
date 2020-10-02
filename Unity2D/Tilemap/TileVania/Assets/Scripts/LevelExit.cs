bdbae05a3dbc4d1fd6183fda3dcd4df9b08fb92d
Barbara Wang <barbara.wang@unity3d.com>
2020-10-02 17:55:37 -0400

    Tilevanian, my fifth game!

---
+++ Unity2D/Tilemap/TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelay = 2f;
+    [SerializeField] float LevelLoadSlowMoFactor = 0.2f;
+
+    private void OnTriggerEnter2D(Collider2D othercollider)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+    
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = LevelLoadSlowMoFactor;
+
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+
+        Time.timeScale = 1f;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
